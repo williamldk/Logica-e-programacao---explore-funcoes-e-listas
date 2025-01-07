0bb2c194cb8927ffed26db4edc739fcc2895d947
williamldk <williamclm_christian@hotmail.com>
2025-01-07 02:28:56 -0300

    Criando uma função que recebe um número como parâmetro e retorna o dobro desse número.

--- Funções/Desafio hora pratica/logica-js-projeto_inicial/app.js
+++ Funções/Desafio hora pratica/logica-js-projeto_inicial/app.js
@@ -7,3 +7,10 @@ function exibirOla() {
   }
   
   exibirOlaNome("Alice");
+
+  function calcularDobro(numero) {
+    return numero * 2;
+  }
+  
+  let resultadoDobro = calcularDobro(5);
+  console.log(resultadoDobro);
