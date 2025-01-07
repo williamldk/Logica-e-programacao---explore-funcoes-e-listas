d553b3a7a6afc72ad625ce96f0d063affee2e066
williamldk <williamclm_christian@hotmail.com>
2025-01-07 02:30:29 -0300

    Criando uma função que recebe três números como parâmetros e retorna a média deles.

--- Funções/Desafio hora pratica/logica-js-projeto_inicial/app.js
+++ Funções/Desafio hora pratica/logica-js-projeto_inicial/app.js
@@ -14,3 +14,10 @@ function exibirOla() {
   
   let resultadoDobro = calcularDobro(5);
   console.log(resultadoDobro);
+
+  function calcularMedia(a, b, c) {
+    return (a + b + c) / 3;
+  }
+  
+  let media = calcularMedia(4, 7, 10);
+  console.log(media);
