99c1c71b9b396fe0b8e494ec85a98b520c3a9110
williamldk <williamclm_christian@hotmail.com>
2025-01-07 00:51:39 -0300

    criando função para ao clicar no botão soma, peça 2 números e exiba o resultado da soma em um alerta.

--- Interagindo com HTML/Desafio hora da pratica/js-curso-2-desafio_1/app.js
+++ Interagindo com HTML/Desafio hora da pratica/js-curso-2-desafio_1/app.js
@@ -13,3 +13,10 @@ function exibirPrompt() {
     let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito:')
     alert(`Estive em ${nomeDaCidade} e lembrei de você`)
 }
+
+function somandoDoisNumeros() {
+    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+    let segundoNumero = parseInt(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+}
