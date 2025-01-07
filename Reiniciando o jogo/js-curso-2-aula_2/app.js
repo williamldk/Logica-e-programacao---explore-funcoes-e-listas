e77a4c0a566526e1c3c5b4789ae4f893651a5a02
williamldk <williamclm_christian@hotmail.com>
2025-01-07 13:56:28 -0300

    Adiciona contagem de tentativas ao jogo e implementa mensagens dinâmicas no singular e plural

--- Reiniciando o jogo/js-curso-2-aula_2/app.js
+++ Reiniciando o jogo/js-curso-2-aula_2/app.js
@@ -1,4 +1,5 @@
-let numeroSecreto = 7;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -13,13 +14,17 @@ function verificarChute() {
 
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!')
-        exibirTextoNaTela('p', 'Você descobriu o número secreto!')
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor!')
         } else {
             exibirTextoNaTela('p', 'O numero secreto é maior!')
         }
+        //tentativas = tentativas + 1;
+        tentativas++;
     }
     console.log(chute == numeroSecreto);
 }
