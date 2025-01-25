d39523491a3e75d3256eb03dda99dbc9f1329228
williamldk <williamclm_christian@hotmail.com>
2025-01-25 15:29:28 -0300

    feat: adicionar funcionalidade para o botão "Novo Jogo"
    - Implementada a função `reiniciarJogo` para zerar tentativas, sortear um novo número secreto e limpar o campo de entrada.
    - Criada a função `exibirMensagemInicial` para isolar o comportamento de exibir as mensagens iniciais, evitando repetição de código.
    - Adicionado comportamento para desabilitar o botão "Novo Jogo" após ser clicado.
    - Chamada inicial de `exibirMensagemInicial` para garantir que as mensagens iniciais sejam exibidas ao carregar o jogo.
    
    Ajustes realizados para melhorar a legibilidade, reusabilidade e funcionamento da aplicação.

--- Reiniciando o jogo/js-curso-2-aula_2/app.js
+++ Reiniciando o jogo/js-curso-2-aula_2/app.js
@@ -6,8 +6,12 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -40,6 +44,14 @@ function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
+
+function reiniciarJogo() {
+        numeroSecreto = gerarNumeroAleatorio();
+        limparCampo();
+        tentativas = 1;
+        exibirMensagemInicial();
+        document.getElementById('reiniciar').setAttribute('disabled', true)
+    }
 
 
 
