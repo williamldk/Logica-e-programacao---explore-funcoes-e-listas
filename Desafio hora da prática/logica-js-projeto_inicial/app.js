70b65b41ccb16239dac43855d6b06a0442c436bb
williamldk <williamclm_christian@hotmail.com>
2025-01-30 12:08:22 -0300

    Criando uma função que calcule o valor do fatorial de um número passado como parâmetro

--- Desafio hora da prática/logica-js-projeto_inicial/app.js
+++ Desafio hora da prática/logica-js-projeto_inicial/app.js
@@ -1,3 +1,21 @@
 function calcularIMC(altura, peso){
     let imc = peso / (alturaMetros * alturaMetros);
 }
+
+function calcularFatorial(numero) {
+    if (numero === 0 || numero === 1) {
+      return 1;
+    }
+  
+    let fatorial = 1;
+    for (let i = 2; i <= numero; i++) {
+      fatorial *= i;
+    }
+  
+    return fatorial;
+  }
+  
+  // Exemplo de uso
+  let numero = 5;
+  let resultado = calcularFatorial(numero);
+  console.log(`O fatorial de ${numero} é ${resultado}`);
