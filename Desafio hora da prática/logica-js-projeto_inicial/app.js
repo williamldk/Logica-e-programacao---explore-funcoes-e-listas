3b5b957ded9a9f91590274066900d2928a05a7e6
williamldk <williamclm_christian@hotmail.com>
2025-01-30 12:23:26 -0300

    Criando função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro

--- Desafio hora da prática/logica-js-projeto_inicial/app.js
+++ Desafio hora da prática/logica-js-projeto_inicial/app.js
@@ -1,7 +1,9 @@
+//função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro
 function calcularIMC(altura, peso){
     let imc = peso / (alturaMetros * alturaMetros);
 }
 
+//função que calcule o valor do fatorial de um número passado como parâmetro.
 function calcularFatorial(numero) {
     if (numero === 0 || numero === 1) {
       return 1;
@@ -20,6 +22,7 @@ function calcularFatorial(numero) {
   let resultado = calcularFatorial(numero);
   console.log(`O fatorial de ${numero} é ${resultado}`);
 
+  //função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$ 4,80
   function converterDolarParaReal(valorEmDolar) {
     let cotacaoDolar = 4.80;
     let valorEmReais = valorEmDolar * cotacaoDolar;
@@ -30,3 +33,18 @@ function calcularFatorial(numero) {
   let valorEmDolar = 50;
   let valorEmReais = converterDolarParaReal(valorEmDolar);
   console.log(`${valorEmDolar} dólares equivalem a R$ ${valorEmReais}`);
+
+  //função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro
+  function calcularAreaPerimetroSalaRetangular(altura, largura) {
+    let area = altura * largura;
+    let perimetro = 2 * (altura + largura);
+    
+    console.log(`Área da sala: ${area} metros quadrados`);
+    console.log(`Perímetro da sala: ${perimetro} metros`);
+  }
+  
+  // Exemplo de uso
+  let altura = 3; // em metros
+  let largura = 5; // em metros
+  calcularAreaPerimetroSalaRetangular(altura, largura);
+  
